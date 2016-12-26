6407fade4e70268d22ab2532d9caa2c8a3df8547
zhangaz1 <zhangaz1@hotmail.com>
2016-12-26 15:33:59 +0800

    update test task

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -1,26 +1,19 @@
 'use strict';
 
 
-module.exports = function (context, name) {
+module.exports = function(context, name) {
+	var tasks = context.config.tasks;
 
 	context.gulp
 		.task(
 			name,
-			'run test task',
-			function (done) {
-				context.gulp
-					.src('test/**/*.test.js')
-					.pipe(context.mocha())
-					// .pipe(context.jasmine())
-					.on('error', function () {
-						console.log(arguments);
-					})
-					.on('end', function (err) {
-						if (err) {
-							console.log(err);
-						}
-						done(null);
-					});
+			'test task',
+			function(done) {
+				context.sequence(
+					tasks.nodeTest,
+					tasks.karmaTest,
+					tasks.protractorTest
+				)(done);
 			}
 		);
 };

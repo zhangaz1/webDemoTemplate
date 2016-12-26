10c7f2c9015351dd6b452b985b699d07b89c23c8
zhangaz1 <zhangaz1@hotmail.com>
2016-12-26 15:26:58 +0800

    add gulpfile.js

--- gulp/context.js
+++ gulp/context.js
@@ -6,12 +6,9 @@ var help = require('gulp-help');
 var debug = require('gulp-debug');
 var sequence = require('gulp-sequence');
 var watch = require('gulp-watch');
-var liveReload = require('gulp-livereload');
 //var exec = require('child_process').exec;
 // var execCmd = require('./../common/cmd/execCmd.js');
 var mocha = require('gulp-mocha');
-var jasmine = require('gulp-jasmine');
-
 
 var config = require('./config.js');
 var cacheManager = require('./cacheManager.js');
@@ -19,15 +16,13 @@ var cacheManager = require('./cacheManager.js');
 gulp = help(gulp);
 
 module.exports = {
-    gulp: gulp,
-    debug: debug,
-    sequence: sequence.use(gulp),
-    liveReload: liveReload,
-    watch: watch,
-    config: config,
-    cacheManager: cacheManager,
-    //exec: exec,
-    // execCmd: execCmd,
-    mocha: mocha,
-    jasmine: jasmine
+	gulp: gulp,
+	debug: debug,
+	sequence: sequence.use(gulp),
+	watch: watch,
+	config: config,
+	cacheManager: cacheManager,
+	//exec: exec,
+	// execCmd: execCmd,
+	mocha: mocha,
 };
